4a8852a4cd7168194867006a656aa9e05178fab0
Arnas Petraitis <arnas.petraitis@visma.com>
2015-12-08 15:55:33 +0200

    Moving in (from OpenESDH-UI)

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# openesdh-addo-ui
+openesdh-addo-ui
+===================
+OpenESDH integration with Visma Addo UI
+
+openesdh-addo-ui
+----------------
+To run project integrated with OpenESDH-UI:
+
+> ../OpenESDH/OpenESDH-UI/app/src/modules > git clone https://github.com/OpenESDH/openesdh-addo-ui.git addo
+
+> ../OpenESDH/OpenESDH-UI > gulp addo [local]
+> or (for all modules)
+> ../OpenESDH/OpenESDH-UI > gulp all [local]
---
+++ addo.module.js
@@ -0,0 +1,10 @@
+angular
+        .module('openeApp.addo', ['pascalprecht.translate'])
+        .config(config);
+
+function config(caseDocumentsSendItemsServiceProvider, languageFilesProvider) {
+    //case menu item
+    caseDocumentsSendItemsServiceProvider.addMenuItem('ADDO.DOCUMENT.SEND_FOR_SIGNING', 'sendToAddoService');
+    //translations
+    languageFilesProvider.addFile('/app/src/modules/addo/i18n/','-addo.json');
+}
