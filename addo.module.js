2098008af1a8d87f3386b16dabc3bda30b90e1c4
Arnas Petraitis <arnas.petraitis@visma.com>
2016-02-11 15:12:59 +0200

    some refactoring

--- addo.module.js
+++ addo.module.js
@@ -2,9 +2,9 @@ angular
         .module('openeApp.addo', ['pascalprecht.translate'])
         .config(config);
 
-function config(caseDocumentsSendItemsServiceProvider, languageFilesProvider) {
+function config(caseDocumentActionsServiceProvider, languageFilesProvider) {
     //case menu item
-    caseDocumentsSendItemsServiceProvider.addMenuItem('ADDO.DOCUMENT.SEND_FOR_SIGNING', 'sendToAddoService', true);
+    caseDocumentActionsServiceProvider.addMenuItem('ADDO.DOCUMENT.SEND_FOR_SIGNING', 'sendToAddoService', true);
     //translations
     languageFilesProvider.addFile('app/src/modules/addo/i18n/','-addo.json');
 }
