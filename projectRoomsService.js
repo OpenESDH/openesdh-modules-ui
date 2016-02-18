7a984ed14c62d0cebc1f7be5e17bfa2306a44883
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-02-18 17:21:29 +0200

    OPENE-463 Project room members management.

--- projectRoomsService.js
+++ projectRoomsService.js
@@ -5,6 +5,7 @@
     function projectRoomsService($http){
         
         return {
+            participantRoles:  ["SiteConsumer", "SiteContributor", "SiteCollaborator", "SiteManager"],
             siteExists: siteExists,
             createSite: createSite,
             getSites: getSites,
@@ -13,7 +14,14 @@
             getSiteDocuments: getSiteDocuments,
             getSiteDocumentsWithAttachments: getSiteDocumentsWithAttachments,
             updateCaseSite: updateCaseSite,
-            closeSite: closeSite
+            closeSite: closeSite,
+            getSiteMembers: getSiteMembers,
+            inviteParticipants: inviteParticipants,
+            getPendingInvites: getPendingInvites,
+            cancelInvite: cancelInvite,
+            removeMember: removeMember,
+            changeMemberRole: changeMemberRole,
+            getInvitationByTicket: getInvitationByTicket
         };
         
         function siteExists(shortName){
@@ -70,4 +78,65 @@
             });
         }
         
+        function getSiteMembers(shortName){
+            return $http.get('/api/sites/' + shortName + '/memberships', {authorityType: 'USER'}).then(function(response){
+                return response.data;
+            });
+        }
+        
+        function inviteParticipants(site){
+            return $http.post('/api/openesdh/case/sites/members/invite', site).then(function(response){
+                return response.data;
+            });
+        }
+        
+        function getPendingInvites(shortName){
+            return $http.get('/api/sites/' + shortName + '/invitations').then(function(response){
+                return response.data.data;
+            });
+        }
+        
+        function cancelInvite(siteShortName, inviteId){
+            return $http.delete('/api/sites/' + siteShortName + '/invitations/' + inviteId).then(function(response){
+                return response.data;
+            });
+        }
+        
+        function acceptInvite(inviteId, inviteTicket, action){
+            return inviteResponse(inviteId, inviteTicket, 'accept');
+        }
+        
+        function declineInvite(){
+            return inviteResponse(inviteId, inviteTicket, 'reject');
+        }
+        
+        function inviteResponse(inviteId, inviteTicket, action){
+            return $http.put('/api/invite/' + inviteId + '/' + inviteTicket + '/' + action).then(function(response){
+                return response.data.data;
+            });
+        }
+        
+        function removeMember(shortName, authority){
+            return $http.delete('/api/sites/' + shortName + '/memberships/' + authority).then(function(response){
+                return response.data;
+            });
+        }
+        
+        function changeMemberRole(shortName, authorityFullName, role){
+            var data = {
+                    role: role,
+                    authority: {
+                        fullName: authorityFullName
+                    }
+            };
+            return $http.put('/alfresco/s/api/sites/' + shortName + '/memberships', data).then(function(response){
+                return response.data;
+            });
+        }
+        
+        function getInvitationByTicket(inviteId, inviteTicket){
+            return $http.get('/alfresco/s/api/invite/' + inviteId + '/' + inviteTicket).then(function(response){
+                return response.data.invite;
+            });
+        }
     }
