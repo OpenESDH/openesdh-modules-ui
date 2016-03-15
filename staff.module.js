10d3263bfca4a2bf80a7974df8eb60d1b6fc79e2
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-03-15 12:51:52 +0200

    OPENE-470 Extracted staff case menu item localization.

--- staff.module.js
+++ staff.module.js
@@ -2,11 +2,13 @@
         .module('openeApp.cases.staff', ['openeApp.cases.common'])
         .config(config);
 
-    function config(caseCrudDialogServiceProvider){
+    function config(caseCrudDialogServiceProvider, languageFilesProvider){
         caseCrudDialogServiceProvider.caseCrudForm({
             type: 'staff:case',
             controller: 'StaffCaseDialogController',
             caseInfoTemplateUrl: 'app/src/modules/staff/view/caseInfo.html'
         });
+        
+        languageFilesProvider.addFile('app/src/modules/staff/i18n/','-staff.json');
     }
 
