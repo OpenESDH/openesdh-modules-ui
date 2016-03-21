9da4757fef4c5bdd89f8fcb77a715b8acb00ed0e
Arnas Petraitis <arnas.petraitis@visma.com>
2016-03-21 17:01:17 +0200

    OPENE-518 edit in google docs

--- ProjectRoomDocumentController.js
+++ ProjectRoomDocumentController.js
@@ -2,9 +2,10 @@
         .module('openeApp.projectRooms')
         .controller('ProjectRoomDocumentController', ProjectRoomDocumentController);
     
-    function ProjectRoomDocumentController($stateParams, projectRoomsService, $controller){
+    function ProjectRoomDocumentController($stateParams, projectRoomsService, $controller, $scope){
         angular.extend(this, $controller('DocumentDetailsController'));
         var vm = this;
+        vm._scope = $scope;
         vm.shortName = $stateParams.shortName;
         vm.activate();
     }
