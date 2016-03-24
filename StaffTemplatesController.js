b5d020fcf4671c5fd741cd82ac99af2a2636d77b
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-03-24 14:25:19 +0200

    OPENE-525 Staff prefilled properties for templates list.

--- StaffTemplatesController.js
+++ StaffTemplatesController.js
@@ -7,6 +7,8 @@
         var vm = this;
         vm.headerMessageKey = 'STAFF_TEMPLATES.ADMIN.SYS_SETTINGS.STAFF_TEMPLATES.STAFF_CASE_TEMPLATES';
         vm.caseType = "staff:case";
-        vm.templateInfoUrl = '#/admin/system-settings/stafftemplate';
+        vm.templateUisref = "administration.systemsettings.stafftemplate"
+        vm.availableProps = vm.availableProps.concat(["staff:hireDate", "staff:resignationDate", "staff:salary"]);
+        vm.propNameKeyPrefixes.push("STAFF_TEMPLATES.PREFILLED.");
         vm.init();
     }
