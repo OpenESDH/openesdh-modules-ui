8516d5592b2325f73a21702cbac19c7f69480c6a
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-03-29 09:37:35 +0300

    OPENE-525 Move case templates localization messages into module.

--- caseTemplates/caseTemplates.module.js
+++ caseTemplates/caseTemplates.module.js
@@ -2,7 +2,10 @@
         .module('openeApp.caseTemplates', [])
         .config(config);
 
-    function config(startCaseWorkflowServiceProvider){
+    function config(startCaseWorkflowServiceProvider, languageFilesProvider){
+        
+        languageFilesProvider.addFile('app/src/modules/caseTemplates/i18n/','-caseTemplates.json');
+        
         startCaseWorkflowServiceProvider.wfSupplier({
             canSupply: function(caseInfo){
                 var template = caseInfo.properties["cm:template"]; 
