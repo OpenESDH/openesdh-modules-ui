a7bf1db4fd6a04d01a013498ec0c7049b8e840e6
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-04-19 11:39:03 +0300

    OPENE-543 Project room documents comments.

--- projectRooms/ProjectRoomDocumentController.js
+++ projectRooms/ProjectRoomDocumentController.js
@@ -3,7 +3,7 @@
         .controller('ProjectRoomDocumentController', ProjectRoomDocumentController);
     
     function ProjectRoomDocumentController($stateParams, projectRoomsService, $controller, $scope){
-        angular.extend(this, $controller('DocumentDetailsController'));
+        angular.extend(this, $controller('DocumentDetailsController', {$scope: $scope}));
         var vm = this;
         vm._scope = $scope;
         vm.shortName = $stateParams.shortName;
