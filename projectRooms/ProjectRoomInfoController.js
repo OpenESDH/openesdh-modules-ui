717dab0d236115cec339ae22d080878b16c26543
Jurij Rudinskij <Jurij.Rudinskij@visma.com>
2016-04-25 12:21:35 +0300

    OPENE-562 A comment to explain empty controller necessity.

--- projectRooms/ProjectRoomInfoController.js
+++ projectRooms/ProjectRoomInfoController.js
@@ -2,4 +2,6 @@
         .module('openeApp.projectRooms')
         .controller('ProjectRoomInfoController', ProjectRoomInfoController);
     
+    // This controller is necessary for project room tabs controls correct working.
+    // Otherwise it ends up opening empty tab contents.
     function ProjectRoomInfoController(){}
